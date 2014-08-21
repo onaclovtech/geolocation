a9c98933b8f27b6961abe929d32b384c1939b6ac
Tyson Bailey <onaclov2000+github@gmail.com>
2014-08-21 11:46:18 -0600

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,16 +1,33 @@
- angular.module('movie', []).factory('$movie', ['$http',
-function($http) {
-return {
-search: function(api_key, query, page_limit) {
-var method = 'JSONP';
-var url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?";
-return $http({
-method: method,
-url: url + "apikey=" + api_key +
-"&q=" + query +// Should I encode query to web stuff or is that taken care of?
-"&page_limit=" + page_limit + '&callback=JSON_CALLBACK'
-});
+// Having some scope issues, it appears I can't access $scope from the callback function passed to watchPosition. I don't know why, if you do, drop me a line or make an issue talking about it.
+angular.module('myapp', [])
+.controller('MyController', ['$scope', function($scope) {
+console.log("Hello");
+$scope.mylocation = "";
+if ("geolocation" in navigator) {
+  var watchID = navigator.geolocation.watchPosition(function(position) {
+  $scope.latitude = position.coords.latitude;
+  $scope.longitude = position.coords.longitude;
+console.log(position.coords.latitude);
+console.log($scope.longitude);
+}, function() {}, {
+  enableHighAccuracy: true, 
+  maximumAge        : 30000, 
+  timeout           : 27000
+} );
+} else {
+  /* geolocation IS NOT available */
 }
-};
-}
-]);
+}]);
+
+
+
+/*
+JSON result will be this for my service when it's created
+ latitude
+    longitude
+    altitude
+    accuracy
+    altitudeAccuracy
+    heading
+    speed
+*/
