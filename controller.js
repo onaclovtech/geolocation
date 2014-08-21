31425f2d65da8d8165ba162d6f5164621ac90c68
Tyson Bailey <onaclov2000+github@gmail.com>
2014-08-21 11:45:46 -0600

    Create controller.js

---
+++ controller.js
@@ -0,0 +1,16 @@
+ angular.module('movie', []).factory('$movie', ['$http',
+function($http) {
+return {
+search: function(api_key, query, page_limit) {
+var method = 'JSONP';
+var url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?";
+return $http({
+method: method,
+url: url + "apikey=" + api_key +
+"&q=" + query +// Should I encode query to web stuff or is that taken care of?
+"&page_limit=" + page_limit + '&callback=JSON_CALLBACK'
+});
+}
+};
+}
+]);
